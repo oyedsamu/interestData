94b0dc009c4484655042ca416f371cf17d440f89
oyedsamu <54637620+oyedsamu@users.noreply.github.com>
2020-04-08 02:18:39 +0100

    Refactored code

--- interest.js
+++ interest.js
@@ -18,34 +18,35 @@ let data = [
 ]
 
 function interestCalculator(arr){
-    let rate=1
-    let interest=0
+    let rate=1;
+    let interest=0;
     let interestData = []
 
     for(let i=0; i<arr.length; i++){
         let principal = arr[i].principal;
         let time = arr[i].time;
     
-    if(principal>= 2500 && (time>1 && time<3)){
-        rate= 3; 
-    } else
-        if (principal >= 2500 && time >= 3) {
-            rate = 4;
-    } else
-        if (principal < 2500 || time <= 1) {
-            rate = 2;
-    } else
-        {
-            rate;
-        }
+        if(principal>= 2500 && (time>1 && time<3)){
+            rate= 3; 
+        } else
+            if (principal >= 2500 && time >= 3) {
+                rate = 4;
+        } else
+            if (principal < 2500 || time <= 1) {
+                rate = 2;
+        } else
+            {
+                rate;
+            }
 
-    interest = (principal * time * rate) / 100;
-    interestData.push({
-        principal: principal,
-        time: time,
-        rate: rate,
-        interest: interest
-    });
+        interest = (principal * time * rate) / 100;
+        
+        interestData.push({
+            principal: principal,
+            time: time,
+            rate: rate,
+            interest: interest
+        });
     }
     console.log(interestData);
     return interestData;
